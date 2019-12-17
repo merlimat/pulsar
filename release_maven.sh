6e2840dc566fc2e6d0d7c331d4217319d6a8e62c
Jerry Peng <jerryp@splunk.com>
2019-12-17 11:17:26 -0800

    modify release script to deploy all artifacts

--- release_maven.sh
+++ release_maven.sh
@@ -2,30 +2,4 @@
 
 set -x -e
 
-ARTIFACTS=(
-    buildtools
-    .
-    pulsar-client
-    pulsar-client-shaded
-    pulsar-client-api
-    pulsar-common
-    pulsar-client-admin
-    pulsar-client-admin-shaded
-    pulsar-functions
-    pulsar-functions/runtime
-    pulsar-functions/instance
-    pulsar-functions/proto
-    pulsar-functions/secrets
-    pulsar-functions/api-java
-    pulsar-functions/utils
-    pulsar-broker-common
-    pulsar-io
-    pulsar-io/core
-    pulsar-io/common
-    pulsar-zookeeper-utils
-)
-
-for DIR in ${ARTIFACTS[@]}; do
-	echo "Processing module :" ${DIR}
-    mvn --settings ./settings.xml -B deploy -DskipTests -DaltDeploymentRepository=releaseRepository::default::http://repo.splunk.com/artifactory/maven-splunk-local -pl ${DIR}
-done
+mvn --settings ./settings.xml -B deploy -DskipTests -DaltDeploymentRepository=releaseRepository::default::http://repo.splunk.com/artifactory/maven-splunk-local
