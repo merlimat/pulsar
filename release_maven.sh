50695712ddaabafba9ab1bdc924a7a68207fc37d
Ali Ahmed <alahmed.se@gmail.com>
2019-12-17 16:08:49 -0800

    Enable batch mode for release script

--- release_maven.sh
+++ release_maven.sh
@@ -2,4 +2,4 @@
 
 set -x -e
 
-mvn --settings ./settings.xml -B deploy -DskipTests -DaltDeploymentRepository=releaseRepository::default::http://repo.splunk.com/artifactory/maven-splunk-local
+mvn -B -ntp --settings ./settings.xml -B deploy -DskipTests -DaltDeploymentRepository=releaseRepository::default::http://repo.splunk.com/artifactory/maven-splunk-local
