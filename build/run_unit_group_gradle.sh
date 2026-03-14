312cdcb9412f245a6cb6e4ef9625b3f85ff3bc92
Matteo Merli <mmerli@apache.org>
2026-03-14 11:31:02 -0700

    Track run_unit_group_gradle.sh in build/ directory

---
+++ build/run_unit_group_gradle.sh
@@ -0,0 +1,193 @@
+#!/usr/bin/env bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+set -e
+set -o pipefail
+set -o errexit
+
+function gradle_test() {
+  (
+    local use_fail_fast=1
+    if [[ "$GITHUB_ACTIONS" == "true" && "$GITHUB_EVENT_NAME" != "pull_request" ]]; then
+      use_fail_fast=0
+    fi
+    if [[ "$1" == "--no-fail-fast" ]]; then
+      use_fail_fast=0
+      shift
+    fi
+    local failfast_args=""
+    if [ $use_fail_fast -eq 1 ]; then
+      failfast_args="-DtestFailFast=true"
+    else
+      failfast_args="-DtestFailFast=false"
+    fi
+    echo "::group::Run tests for " "$@"
+    ./gradlew "$@" $failfast_args "${COMMANDLINE_ARGS[@]}"
+    echo "::endgroup::"
+    set +x
+    "$SCRIPT_DIR/pulsar_ci_tool.sh" move_test_reports
+  )
+}
+
+# solution for printing output in "set -x" trace mode without tracing the echo calls
+shopt -s expand_aliases
+echo_and_restore_trace() {
+  builtin echo "$@"
+  [ $trace_enabled -eq 1 ] && set -x || true
+}
+alias echo='{ [[ $- =~ .*x.* ]] && trace_enabled=1 || trace_enabled=0; set +x; } 2> /dev/null; echo_and_restore_trace'
+
+# Test Groups  -- start --
+function test_group_broker_group_1() {
+  gradle_test :pulsar-broker:test -PtestGroups='broker'
+  # run broker-isolated test individually
+  gradle_test :pulsar-broker:test -PexcludedTestGroups='' \
+    --tests 'org.apache.pulsar.broker.stats.prometheus.PrometheusMetricsGeneratorWithNoUnsafeTest'
+}
+
+function test_group_broker_group_2() {
+  gradle_test :pulsar-broker:test \
+    -PtestGroups='schema,utils,functions-worker,broker-io,broker-discovery,broker-compaction,broker-naming,websocket,other,builtin,stats'
+}
+
+function test_group_broker_group_3() {
+  gradle_test :pulsar-broker:test -PtestGroups='broker-admin'
+  # run AdminApiTransactionMultiBrokerTest independently with a larger heap size
+  gradle_test :pulsar-broker:test -PexcludedTestGroups='' \
+    --tests 'org.apache.pulsar.broker.admin.v3.AdminApiTransactionMultiBrokerTest' \
+    -DmaxHeapSize=1500m
+}
+
+function test_group_broker_group_4() {
+  gradle_test :pulsar-broker:test -PtestGroups='cluster-migration'
+}
+
+function test_group_broker_group_5() {
+  gradle_test :pulsar-broker:test -PtestGroups='broker-replication'
+}
+
+function test_group_broker_client_api() {
+  gradle_test :pulsar-broker:test -PtestGroups='broker-api'
+}
+
+function test_group_broker_client_impl() {
+  gradle_test :pulsar-broker:test -PtestGroups='broker-impl'
+}
+
+function test_group_client() {
+  gradle_test :pulsar-client:test
+}
+
+function test_group_metadata() {
+  gradle_test :pulsar-metadata:test
+}
+
+function test_group_proxy() {
+  echo "::group::Running pulsar-proxy tests"
+  gradle_test :pulsar-proxy:test
+  echo "::endgroup::"
+}
+
+function test_group_broker_flaky() {
+  echo "::endgroup::"
+  echo "::group::Running quarantined tests"
+  gradle_test --no-fail-fast :pulsar-broker:test \
+    -PtestGroups='quarantine' -PexcludedTestGroups='flaky' || true
+  echo "::endgroup::"
+  echo "::group::Running flaky tests"
+  gradle_test --no-fail-fast :pulsar-broker:test \
+    -PtestGroups='flaky' -PexcludedTestGroups='quarantine'
+  echo "::endgroup::"
+}
+
+# The OTHER group runs tests in all modules except broker, proxy, client, metadata, and IO modules
+function test_group_other() {
+  # Run tests in all modules except the ones that have dedicated CI groups
+  gradle_test \
+    --exclude-task :pulsar-broker:test \
+    --exclude-task :pulsar-proxy:test \
+    --exclude-task :pulsar-client:test \
+    --exclude-task :pulsar-metadata:test \
+    test
+
+  # Run DnsResolverTest separately since it relies on static field values
+  gradle_test :pulsar-common:test \
+    -PexcludedTestGroups='' \
+    --tests 'org.apache.pulsar.common.util.DnsResolverTest'
+}
+
+function test_group_pulsar_io() {
+  echo "::group::Running pulsar-io tests"
+  # Pulsar IO connector tests not yet migrated to Gradle
+  echo "::warning::Pulsar IO tests are not yet available in the Gradle build"
+  echo "::endgroup::"
+}
+
+function test_group_pulsar_io_elastic() {
+  echo "::group::Running elastic-search tests"
+  echo "::warning::Pulsar IO Elastic tests are not yet available in the Gradle build"
+  echo "::endgroup::"
+}
+
+function test_group_pulsar_io_kafka_connect() {
+  echo "::group::Running Pulsar IO Kafka connect adaptor tests"
+  echo "::warning::Pulsar IO Kafka Connect tests are not yet available in the Gradle build"
+  echo "::endgroup::"
+}
+
+function test_group_protobufv4() {
+  echo "::group::Running Protobuf v4 tests"
+  echo "::warning::Protobuf v4 tests are not yet available in the Gradle build (requires rebuild with different protobuf version)"
+  echo "::endgroup::"
+}
+
+function list_test_groups() {
+  declare -F | awk '{print $NF}' | sort | grep -E '^test_group_' | sed 's/^test_group_//g' | tr '[:lower:]' '[:upper:]'
+}
+
+# Test Groups  -- end --
+
+if [[ "$1" == "--list" ]]; then
+  list_test_groups
+  exit 0
+fi
+
+TEST_GROUP=$1
+if [ -z "$TEST_GROUP" ]; then
+  echo "usage: $0 [test_group]"
+  echo "Available test groups:"
+  list_test_groups
+  exit 1
+fi
+shift
+COMMANDLINE_ARGS=("$@")
+echo "Test Group : $TEST_GROUP"
+test_group_function_name="test_group_$(echo "$TEST_GROUP" | tr '[:upper:]' '[:lower:]')"
+if [[ "$(LC_ALL=C type -t "${test_group_function_name}")" == "function" ]]; then
+  set -x
+  eval "$test_group_function_name"
+else
+  echo "INVALID TEST GROUP"
+  echo "Available test groups:"
+  list_test_groups
+  exit 1
+fi
